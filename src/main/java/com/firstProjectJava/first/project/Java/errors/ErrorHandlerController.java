2a91c3221affb0460a15b91455123e5aa7189620
Itamar Nascimento <itamarasa@gmail.com>
2025-01-06 14:05:36 -0300

    removed empty message  and add new execptions handles

--- src/main/java/com/firstProjectJava/first/project/Java/errors/ErrorHandlerController.java
+++ src/main/java/com/firstProjectJava/first/project/Java/errors/ErrorHandlerController.java
@@ -1,6 +1,9 @@
 package com.firstProjectJava.first.project.Java.errors;
 
 import com.firstProjectJava.first.project.Java.dtos.ErrorResponse;
+import com.firstProjectJava.first.project.Java.exceptions.ExceptionCannotRemove;
+import com.firstProjectJava.first.project.Java.exceptions.ExceptionNotFound;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,18 +13,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ErrorHandlerController {
 
 
-
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorResponse<String>> handleException(Exception e){
+  public ResponseEntity<ErrorResponse<String>> handleException(Exception e) {
     return ResponseEntity.status(500).body(new ErrorResponse<>(e.getMessage()));
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<ErrorResponse<?>> handleException(MethodArgumentNotValidException e){
-    return ResponseEntity.status(400).body(new ErrorResponse<>(e.getDetailMessageArguments()));
+  public ResponseEntity<ErrorResponse<?>> handleException(MethodArgumentNotValidException e) {
+    var errors = e.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
+    return ResponseEntity.status(400).body(new ErrorResponse<>(errors));
   }
 
+  @ExceptionHandler(ExceptionNotFound.class)
+  public ResponseEntity<ErrorResponse<?>> handleException(ExceptionNotFound e) {
+    return ResponseEntity.status(404).body(new ErrorResponse<>(e.getMessage()));
+  }
 
+  @ExceptionHandler(ExceptionCannotRemove.class)
+  public ResponseEntity<ErrorResponse<?>> handleException(ExceptionCannotRemove e) {
+    return ResponseEntity.status(409).body(new ErrorResponse<>(e.getMessage()));
+  }
 }
 
 
