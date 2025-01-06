5d77a4ad7dcceb29ae22a099d47d4833fd7a9c94
Itamar Nascimento <itamarasa@gmail.com>
2025-01-06 14:02:38 -0300

    Create ExceptionCannotRemove

---
+++ src/main/java/com/firstProjectJava/first/project/Java/exceptions/ExceptionCannotRemove.java
@@ -0,0 +1,7 @@
+package com.firstProjectJava.first.project.Java.exceptions;
+
+public class ExceptionCannotRemove extends RuntimeException {
+  public ExceptionCannotRemove(String message) {
+    super(message);
+  }
+}
