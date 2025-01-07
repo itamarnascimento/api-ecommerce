f8e8cadaa9c1eecc08fa7223c3b2f8a2f7e02a8f
Itamar Nascimento <itamarasa@gmail.com>
2025-01-07 16:56:50 -0300

    add parament de success

--- src/main/java/com/firstProjectJava/first/project/Java/dtos/ErrorResponse.java
+++ src/main/java/com/firstProjectJava/first/project/Java/dtos/ErrorResponse.java
@@ -1,3 +1,7 @@
 package com.firstProjectJava.first.project.Java.dtos;
 
-public record ErrorResponse<T>(T message) {}
+public record ErrorResponse<T>(T message, boolean success) {
+  public ErrorResponse(T message) {
+    this(message, false);
+  }
+}
