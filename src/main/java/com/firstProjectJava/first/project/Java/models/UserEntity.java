1eeb20dcf90aa82500dbe283828f09b4f5019d1e
Itamar Nascimento <itamarasa@gmail.com>
2025-01-07 16:56:06 -0300

    add lock de route private

--- src/main/java/com/firstProjectJava/first/project/Java/models/UserEntity.java
+++ src/main/java/com/firstProjectJava/first/project/Java/models/UserEntity.java
@@ -5,6 +5,7 @@ import com.firstProjectJava.first.project.Java.models.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -47,7 +48,7 @@ public class UserEntity implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
+    return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
   }
 
   @Override
